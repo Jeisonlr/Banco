ae689196a68057c9164d161ccb982e21dc4a9df9
nicolas <nicolasmahecha1125@gmail.com>
2023-10-16 11:38:29 -0500

    hola

---
+++ src/main/java/com/example/ProyectoBancoJPA/ProyectoBancoJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoBancoJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBancoJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBancoJpaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/model/Bolsillo.java
@@ -0,0 +1,37 @@
+package model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "bolsillo")
+public class Bolsillo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "bolsillo_generator",allocationSize = 1)
+    private Long id;
+    @Column(name ="saldo",nullable = false)
+    private Double saldo;
+    @Column(name = "nombreBolsillo",nullable = false)
+    private String nobreBolsillo;
+    @Column(name = "estado",nullable = false)
+    private String estado;
+
+    public Bolsillo() {
+    }
+
+    public Bolsillo(Long idBolsillo, Double saldo, String nobreBolsillo) {
+        this.id = idBolsillo;
+        this.saldo = saldo;
+        this.nobreBolsillo = nobreBolsillo;
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Bolsillo{" +
+                "id=" + id +
+                ", saldo=" + saldo +
+                ", nobreBolsillo='" + nobreBolsillo + '\'' +
+                '}';
+    }
+}
