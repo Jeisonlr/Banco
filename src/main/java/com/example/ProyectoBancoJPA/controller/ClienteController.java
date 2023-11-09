4472e3f70b62f76e1378bdbfdeef6ad78ca87e2f
Sof-Saos <sofisalamanca93@gmail.com>
2023-11-09 10:35:22 -0500

    merge and conflict resolution between Jeison's and my branch Adding of initial Swagger documentation

--- src/main/java/com/example/ProyectoBancoJPA/controller/ClienteController.java
+++ src/main/java/com/example/ProyectoBancoJPA/controller/ClienteController.java
@@ -2,13 +2,7 @@ package com.example.ProyectoBancoJPA.controller;
 
 import com.example.ProyectoBancoJPA.model.Cliente;
 import com.example.ProyectoBancoJPA.service.ClienteService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,7 +10,6 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/clientes")
-@Api(value = "Clientes", description = "Controladores asociados a los clientes")
 public class ClienteController {
     private final ClienteService clienteService;
 
@@ -36,12 +29,6 @@ public class ClienteController {
     }
 
     @PostMapping ("/create")
-    @ApiOperation(value = "Crear un cliente", response = Cliente.class)
-    @ApiResponses(value = {
-            @ApiResponse(code = 201, message = "El cliente ha sido creado exitosamente", response = Cliente.class),
-            @ApiResponse(code = 400, message = "La solicitued es incorrecta", response = ErrorResponse.class),
-            @ApiResponse(code = 500, message = "Error interno del servidor", response = ErrorResponse.class)
-    })
     public Cliente crearCliente(@RequestBody Cliente cliente){
         return this.clienteService.createCliente(cliente);
     }
